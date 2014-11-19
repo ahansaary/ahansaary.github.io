8ed1165ba6dd745d07d70f70c4c5d3e7fd5816be
Ahmed Al Ansaary <ahansaary@gmail.com>
2014-11-19 23:29:46 +0200

    [] to array() in data.php

--- projects/highcharts/data.php
+++ projects/highcharts/data.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-$data = [];
+$data = array();
 foreach(file('data.txt') as $line) {
 	$data['s1'][] = explode(',', $line)[0]; 
 	$data['s2'][] = explode(',', $line)[1]; 
