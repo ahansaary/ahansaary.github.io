5733bef7a85c8801ffaf6dfbedcb4c202c41480a
Ahmed Al Ansaary <ahansaary@gmail.com>
2014-11-19 22:55:38 +0200

    add highcharts project

---
+++ projects/highcharts/data.php
@@ -0,0 +1,10 @@
+<?php
+header('Content-Type: application/json');
+$data = [];
+foreach(file('data.txt') as $line) {
+	$data['s1'][] = explode(',', $line)[0]; 
+	$data['s2'][] = explode(',', $line)[1]; 
+	$data['s3'][] = explode(',', $line)[2]; 
+}
+echo json_encode($data);
+//print_r($data);
