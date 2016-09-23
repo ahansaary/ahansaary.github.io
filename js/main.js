a8ef08b441b8207aeb25213e0d7ec07c96082d65
Ahmed Al Ansaary <ahansaary@gmail.com>
2016-09-23 07:09:53 +0200

    Initial Commit for the new design

--- js/main.js
+++ js/main.js
@@ -1,72 +1,76 @@
-(function() {
-	$('.image')
-	//.hide()
-	.on('click', function() {
-		$(this).addClass('tada');
-		setTimeout(function() { $('.image').removeClass('tada'); }, 1000);
-	});
-	$('.name')
-	//.hide()
-	.on('click', function() {
-		$(this).addClass('flip');
-		setTimeout(function() { $('.name').removeClass('flip'); }, 1000);
-	});
-	$('th')
-	//.hide()
-	.on('mouseover', function() {
-		$(this).addClass('animated wobble');
-		setTimeout(function() { $('th').removeClass('animated wobble'); }, 1000);
-	});
-	i=0;
-	interval = setInterval(function() {
-		if(i==0) {
-			$('.image')
-			.show()
-			.addClass('animated flipInX');
-			setTimeout(function() {
-				$('.image')
-				.removeClass('flipInX')
-				.addClass('rubberBand');
-				setTimeout(function() { $('.image').removeClass('rubberBand'); }, 1000);
-			}, 3000);
-			i = i+1;
-		} else if(i==1) {
-			$('.name')
-			.show()
-			.addClass('animated flipInY');
-			setTimeout(function() { $('.name').removeClass('flipInY'); }, 1000);
-			i = i+1;
-		} else if(i==2) {
-			$('.card')
-			.show()
-			.addClass('animated flipInY');
-			setTimeout(function() { $('.card').removeClass('flipInY'); }, 1000);
-			i = i+1;
-		} else if(i==3) {
-			$('.skills')
-			.show()
-			.addClass('animated flipInY');
-			setTimeout(function() { $('.skills').removeClass('flipInY'); }, 1000);
-			i = i+1;
-			scroll = setInterval(function() {
-				if($(window).scrollTop() == $(document).height() - $(window).height()) {
-					clearInterval(scroll);
-					$("html, body").animate({scrollTop : 0},800);
-				} else {
-					$(window).scrollTop($(window).scrollTop() + 1);
-				}
-			}, 10);
-		} else if(i==4) {
-			$('.activities')
-			.show()
-			.addClass('animated flipInY');
-			setTimeout(function() { $('.activities').removeClass('flipInY'); }, 1000);
-			i = i+1;
-		} else {
-			clearInterval(interval);
-		}
-	}, 1000);
-	
-	$("*").bind("contextmenu",function(){ return false; });
-	$("*").on('dragstart', function(event) { event.preventDefault(); });
-}());
+jQuery(document).ready(function ($) {
+  var gallery = $('.cd-gallery'),
+    foldingPanel = $('.cd-folding-panel'),
+    mainContent = $('.cd-main');
+  /* open folding content */
+  gallery.on('click', 'a', function (event) {
+    event.preventDefault();
+    openItemInfo($(this).attr('href'));
+  });
+
+  /* close folding content */
+  foldingPanel.on('click', '.cd-close', function (event) {
+    event.preventDefault();
+    toggleContent('', false);
+  });
+  gallery.on('click', function (event) {
+    /* detect click on .cd-gallery::before when the .cd-folding-panel is open */
+    if ($(event.target).is('.cd-gallery') && $('.fold-is-open').length > 0) toggleContent('', false);
+  });
+
+  function openItemInfo(url) {
+    var mq = viewportSize();
+    if (gallery.offset().top > $(window).scrollTop() && mq != 'mobile') {
+      /* if content is visible above the .cd-gallery - scroll before opening the folding panel */
+      $('body,html').animate({
+        'scrollTop': gallery.offset().top
+      }, 100, function () {
+        toggleContent(url, true);
+      });
+    } else if (gallery.offset().top + gallery.height() < $(window).scrollTop() + $(window).height() && mq != 'mobile') {
+      /* if content is visible below the .cd-gallery - scroll before opening the folding panel */
+      $('body,html').animate({
+        'scrollTop': gallery.offset().top + gallery.height() - $(window).height()
+      }, 100, function () {
+        toggleContent(url, true);
+      });
+    } else {
+      toggleContent(url, true);
+    }
+  }
+
+  function toggleContent(url, bool) {
+    if (bool) {
+      /* load and show new content */
+      var foldingContent = foldingPanel.find('.cd-fold-content');
+      foldingContent.load(url + ' .cd-fold-content > *', function (event) {
+        setTimeout(function () {
+          $('body').addClass('overflow-hidden');
+          foldingPanel.addClass('is-open');
+          mainContent.addClass('fold-is-open');
+        }, 100);
+
+      });
+    } else {
+      /* close the folding panel */
+      var mq = viewportSize();
+      foldingPanel.removeClass('is-open');
+      mainContent.removeClass('fold-is-open');
+
+      (mq == 'mobile' || $('.no-csstransitions').length > 0 )
+        /* according to the mq, immediately remove the .overflow-hidden or wait for the end of the animation */
+        ? $('body').removeClass('overflow-hidden')
+
+        : mainContent.find('.cd-item').eq(0).one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+        $('body').removeClass('overflow-hidden');
+        mainContent.find('.cd-item').eq(0).off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+      });
+    }
+
+  }
+
+  function viewportSize() {
+    /* retrieve the content value of .cd-main::before to check the actua mq */
+    return window.getComputedStyle(document.querySelector('.cd-main'), '::before').getPropertyValue('content').replace(/"/g, "").replace(/'/g, "");
+  }
+});
